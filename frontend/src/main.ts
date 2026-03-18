26593b9565f781b58ec1770fba09831feb003d70
zhongterry <panxh09@sina.com>
2026-03-18 19:48:16 +0800

    init vite and vue and shadcnui

---
+++ frontend/src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import App from './App.vue'
+import './assets/main.css' // 确保在这里引入
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            // 关键：将 PrimeVue 样式放在 css 层级中，方便 Tailwind 覆盖
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            },
+            darkModeSelector: '.dark'
+        }
+    }
+})
+
+app.mount('#app')
