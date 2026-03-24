189f10d14615aa37d686e94c022ba391fb1709fe
Colinxu2020 <colinxu2020@gmail.com>
2026-03-24 19:48:33 +0800

    Deal with some issues.

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -3,7 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
-import eslintConfigPrettier = require("eslint-config-prettier");
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
