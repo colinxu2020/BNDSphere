d081ef530ad340794a7814d3a4d47937e1451f55
zhongterry <ztrztr2011@163.com>
2026-03-27 16:39:45 +0800

    修复部分错别字

--- frontend/src/layouts/GuestLayout.vue
+++ frontend/src/layouts/GuestLayout.vue
@@ -6,7 +6,7 @@ import NavItem from '@/components/TopBar/NavItem.vue';
   <header class="h-16 border-b border-gray-200 px-32">
     <div class="flex h-full items-center">
       <!-- 左侧 -->
-      <div class="w-40 font-semibold text-gray-800">一学校云平台</div>
+      <div class="w-40 font-semibold text-gray-800">十一学校云平台</div>
 
       <!-- 中间 -->
       <nav class="flex flex-1 items-center justify-center gap-8">
