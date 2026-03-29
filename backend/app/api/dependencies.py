393da2c343210828abde078b52c379104a30994b
Colinxu2020 <colinxu2020@gmail.com>
2026-03-29 16:56:15 +0800

    :label: Improve Type Hints

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -56,9 +56,9 @@ class RoleChecker:
         return user
 
 
-class ServiceFactory:
-    def __init__(self, typ: type[ServiceBase]) -> None:
+class ServiceFactory[Service: ServiceBase]:
+    def __init__(self, typ: type[Service]) -> None:
         self.typ = typ
 
-    def __call__(self, db: Annotated[AsyncSession, Depends(get_db)]) -> ServiceBase:
+    def __call__(self, db: Annotated[AsyncSession, Depends(get_db)]) -> Service:
         return self.typ(db)
