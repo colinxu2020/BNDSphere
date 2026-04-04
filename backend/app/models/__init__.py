c2b9eba40bc5fa94a083f272bb3bd4416f247cff
Colinxu2020 <colinxu2020@gmail.com>
2026-04-04 22:56:00 +0800

    :bug: Fix Activity Issue

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,9 @@
+from app.models.activity import Activity
+from app.models.activity_participator import activity_participator_table  # noqa: F401
 from app.models.club import Club
 from app.models.clubmember import ClubMember
 from app.models.clubtag import club_tag_table  # noqa: F401
 from app.models.tag import Tag
 from app.models.user import User
 
-__all__ = ["Club", "ClubMember", "Tag", "User"]
+__all__ = ["Activity", "Club", "ClubMember", "Tag", "User"]
