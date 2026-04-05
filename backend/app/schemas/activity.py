738e7d75b9c7b40af78c123bbad88f0b9989bd95
tongtongchuan <tonyytwang@gmail.com>
2026-04-05 23:50:44 +0800

    :bug: fix settings and constants

--- backend/app/schemas/activity.py
+++ backend/app/schemas/activity.py
@@ -2,19 +2,19 @@ from datetime import datetime
 
 from pydantic import BaseModel, Field
 
-from app.core.settings import settings
+from app.core import constants
 from app.models.activity import ActivityStatusEnum
 
 
 class ActivityBase(BaseModel):
     id: int
-    name: str = Field(..., max_length=settings.activity_max_name_length)
+    name: str = Field(..., max_length=constants.ACTIVITY_MAX_NAME_LENGTH)
     club_id: int
 
 
 class ActivityInfo(ActivityBase):
-    description: str = Field(..., max_length=settings.activity_max_description_length)
-    location: str = Field(..., max_length=settings.activity_max_location_length)
+    description: str = Field(..., max_length=constants.ACTIVITY_MAX_DESCRIPTION_LENGTH)
+    location: str = Field(..., max_length=constants.ACTIVITY_MAX_LOCATION_LENGTH)
     start_time: datetime
     end_time: datetime
     status: ActivityStatusEnum
@@ -22,14 +22,14 @@ class ActivityInfo(ActivityBase):
 
 
 class ActivityCreate(BaseModel):
-    name: str = Field(..., max_length=settings.activity_max_name_length)
+    name: str = Field(..., max_length=constants.ACTIVITY_MAX_NAME_LENGTH)
     club_id: int
-    description: str = Field(..., max_length=settings.activity_max_description_length)
-    location: str = Field(..., max_length=settings.activity_max_location_length)
+    description: str = Field(..., max_length=constants.ACTIVITY_MAX_DESCRIPTION_LENGTH)
+    location: str = Field(..., max_length=constants.ACTIVITY_MAX_LOCATION_LENGTH)
     start_time: datetime
     end_time: datetime
 
 
 class ActivityUpdate(BaseModel):
-    description: str = Field(..., max_length=settings.activity_max_description_length)
+    description: str = Field(..., max_length=constants.ACTIVITY_MAX_DESCRIPTION_LENGTH)
     picture_urls: list[str] | None
