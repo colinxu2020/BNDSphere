0b654718d65fc68df39de57bdd261129ed084e0c
Colinxu2020 <colinxu2020@gmail.com>
2026-04-07 12:44:29 +0800

    :fire: Remove Unused Service

--- backend/app/services/club.py
+++ backend/app/services/club.py
@@ -120,12 +120,6 @@ class ClubMemberService(ServiceBase[ClubMember, ClubMemberUpdate, ClubMemberUpda
         )
         return result.scalars().first()
 
-    async def is_club_admin(self, club: Club, user: User) -> bool:
-        result = await self.get_by_club_user(club, user)
-        if result is None:
-            return False
-        return result in {ClubMembershipEnum.president, ClubMembershipEnum.vice}
-
     async def set_relationship(
         self,
         club: Club,
